2b751ed9a2932d6960426f134498948e38404372
Thugg55 <thuggeru7@gmail.com>
2024-01-23 05:51:57 -0800

    bubbles

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,8 +8,8 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, passes = 1;
-	int repeat = 1, swap = 0;
+	size_t i, pass = 1;
+	int repeat = 1, swp = 0;
 
 	if (array == NULL || size < 2)
 		return;
@@ -17,17 +17,17 @@ void bubble_sort(int *array, size_t size)
 	while (repeat == 1)
 	{
 		repeat = 0;
-		for (i = 0; i < (size - passes); i++)
+		for (i = 0; i < (size - pass); i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap = array[i];
+				swp = array[i];
 				array[i] = array[i + 1];
-				array[i + 1] = swap;
+				array[i + 1] = swp;
 				repeat = 1;
 				print_array(array, size);
 			}
 		}
-		passes++;
+		pass++;
 	}
 }
